38e55fed1b6a686c86ad520bfdb012f0f0652d63
Matthew <ricklefsmatthew1003@gmail.com>
2022-10-06 12:05:12 -0500

    prepares README with informatino about what memoization is in React and generale instances when it would be useful or unnecessary, provides examples of when to use it to boost performance and when it would be unnecessary, updates params object to deepen our useMemo example..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { useState, memo } from 'react'
+import { useState, memo, useMemo } from 'react'
 import './App.css'
 
-function Swatch({ color }) {
-  console.log(`Swatch rendered ${color}`)
+function Swatch({ params }) {
+  console.log(`Swatch rendered ${params.color}`)
 
   return (
     <div
-      style={{ margin: 2, width: 75, height: 75, backgroundColor: color }}
+      style={{
+        margin: 2,
+        width: 75,
+        height: 75,
+        backgroundColor: params.color,
+      }}
     ></div>
   )
 }
@@ -15,7 +20,20 @@ const MemoedSwatch = memo(Swatch)
 
 function App() {
   const [appRenderIndex, setAppRenderIndex] = useState(0)
+  const [color, setColor] = useState('red')
+
   console.log(`App rendered ${appRenderIndex}`)
+
+  const params = useMemo(() => ({ color }), [color])
+
+  // Bad useMemo example..
+  // let number1, number2
+  // const value = useMemo(() => number1 + number2, [number1, number2])
+  // const value is a primitive value.. a primitive value being a number, boolean, or string
+  // is always going to compare by value.. so you are going to increase the performance overhead for looking at the dependency array.. running the function.. and cacheing the result...
+
+  // Good useMemo example
+  // const value = number1 + number2
 
   return (
     <div className="App">
@@ -23,10 +41,14 @@ function App() {
         <button onClick={() => setAppRenderIndex(appRenderIndex + 1)}>
           Re-Render App
         </button>
+
+        <button onClick={() => setColor(color === 'red' ? 'blue' : 'red')}>
+          Change Color
+        </button>
       </div>
 
       <div>
-        <MemoedSwatch color="red" />
+        <MemoedSwatch params={params} />
       </div>
     </div>
   )
