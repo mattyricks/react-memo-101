2fa2c3a30e65d2876919d68008cecbc7c2351f2d
Matthew <ricklefsmatthew1003@gmail.com>
2022-10-06 11:09:31 -0500

    cleans up App.js, utilizes state to count rerenders, creates swatch component to utilize app state, uses Higher Order function, memo, to begin testing swatch to maintain the swatch's state and only rerender the swatch one time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, memo } from 'react'
+import './App.css'
 
-function App() {
+function Swatch({ color }) {
+  console.log(`Swatch rendered ${color}`)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div
+      style={{ margin: 2, width: 75, height: 75, backgroundColor: color }}
+    ></div>
+  )
 }
 
-export default App;
+const MemoedSwatch = memo(Swatch)
+
+function App() {
+  const [appRenderIndex, setAppRenderIndex] = useState(0)
+  console.log(`App rendered ${appRenderIndex}`)
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={() => setAppRenderIndex(appRenderIndex + 1)}>
+          Re-Render App
+        </button>
+      </div>
+
+      <div>
+        <MemoedSwatch color="red" />
+      </div>
+    </div>
+  )
+}
+
+export default App
